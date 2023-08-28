87949092aa4fd98bb5562e6b7a5b949e9df184b9
Tom L <tomhasmail99@gmail.com>
2023-08-28 23:10:02 +0100

    fix issues caused by merge

--- src/Meditelligence.DataAccess/Migrations/20230828214034_removeSeedDataAndMoveToProduction.cs
+++ src/Meditelligence.DataAccess/Migrations/20230828214034_removeSeedDataAndMoveToProduction.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Diagnostics.CodeAnalysis;
 
 #nullable disable
 
@@ -7,6 +8,7 @@
 namespace Meditelligence.DataAccess.Migrations
 {
     /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
     public partial class removeSeedDataAndMoveToProduction : Migration
     {
         /// <inheritdoc />
--- src/Meditelligence.DataAccess/Repositories/UserRepo.cs
+++ src/Meditelligence.DataAccess/Repositories/UserRepo.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Meditelligence.DataAccess.Repositories
 {
-    public class UserRepo :IUserRepo
+    public class UserRepo : IUserRepo
     {
         private readonly MeditelligenceDBContext _context;
 
